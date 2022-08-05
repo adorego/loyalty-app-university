7013f6f026949aa0ec8e2c20fae77f9c05163dfa
Andres Do Rego Barros <adoregoel@gmail.com>
2022-08-04 21:29:40 -0400

    Portal ready

--- common/Layout/MainLayout.tsx
+++ common/Layout/MainLayout.tsx
@@ -14,10 +14,13 @@ const MainLayout = ({children,...props}:MainLayoutProps) =>{
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta charSet="utf-8" />
                 <meta property="og:url" content={'https://www.loyaltyapp.com.py'} key="ogurl" />
-                <meta property="og:image" content={'https://loyaltyapp.com.py/images/social_image.png'} key="ogimage" />
-                <meta property="og:site_name" content={"LoyaltyApp"} key="ogsitename" />
+                <meta property="og:image" itemProp="image" content={'https://loyaltyapp.com.py/images/social_image.png'} key="ogimage" />
+                <meta property="og:type" content="website" />
+                <meta property="og:updated_time" content="1440432930" />
+                <meta property="og:site_name" content={"LoyaltyApp, Innovación en la Educación"} key="ogsitename" />
                 <meta property="og:title" content={"LoyaltyAPP Universidad, APP de matriculación digital, Innovación en la Educación"} key="ogtitle" />
                 <meta property="og:description" content={"LoyaltyAPP es una plataforma de creación de APPs de Ventas Digitales y Fidelización"} key="ogdesc" />
+            
             </Head>
             <header>
                 <HomeHeader />
